6375a4cf0833cd0b81eb4186dbdb481def07f036
DariuszSikorra <dariusz.sikorra@gmail.com>
2024-05-02 07:41:52 +0200

    config fixes2

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       'helpers': path.resolve(__dirname, 'src/helpers'),
     },
   },
-  base: "https://<USERNAME>.github.io/portfolio-mk4/"
+  base: "https://dariuszsikorra.github.io/portfolio-mk4/"
 })
